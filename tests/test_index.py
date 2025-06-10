1c41bf47a9aa69d0456e2dc16b9b3e4fa3572244
mariia-shvets-23 <marishvets23@gmail.com>
2025-06-10 21:25:43 +0300

    Add tests+ requirements + Jenkins dockerfile

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,5 +1,6 @@
 import pathlib, re
 
+#test_title func
 def test_title_present():
     html = pathlib.Path("index.html").read_text(encoding="utf-8")
     assert re.search(r"<title>.+?</title>", html, re.I), "index.html must contain <title>"
